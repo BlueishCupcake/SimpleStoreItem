7296613651732ec8fee3e2567f1ba56c8f2c3a84
Sophie <sophiequines@gmail.com>
2021-09-01 17:21:36 -0300

    Adding products info in the cards.

--- src/components/SimpleCard/index.tsx
+++ src/components/SimpleCard/index.tsx
@@ -2,7 +2,40 @@
 import * as S from "./style";
 
 const SimpleCard = () => {
-  return <S.CardContainer>Hellow, I'm a card uwu</S.CardContainer>;
+  const cards = [
+    {
+      name: "Monitor LED 27'' Gamer Curvo Samsung  1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50",
+      price: 2813.99,
+      currentPrice: 2599.0,
+      displayInstallmentPrice: 259.9,
+      img: "/first-screen.png",
+    },
+    {
+      name: "Monitor Gamer LG 25UM58G 25” LED IPS",
+      price: 1599.0,
+      currentPrice: 899.0,
+      displayInstallmentPrice: 109.9,
+      img: "/second-screen.jpg",
+    },
+  ];
+
+  return (
+    <>
+      {cards.map((item) => {
+        return (
+          <S.CardContainer>
+            <S.ProductImg src={item.img} alt="Product Image" />
+            <S.ProductTitle>{item.name}</S.ProductTitle>
+            <S.FullPriceStyled>R$ {item.price}</S.FullPriceStyled>
+            <S.CurrentPriceStyled>R$ {item.currentPrice}</S.CurrentPriceStyled>
+            <S.PaymentOptions>
+              em até 10x de R$ {item.displayInstallmentPrice} sem juros
+            </S.PaymentOptions>
+          </S.CardContainer>
+        );
+      })}
+    </>
+  );
 };
 
 export default SimpleCard;
--- src/components/SimpleCard/style.ts
+++ src/components/SimpleCard/style.ts
@@ -1,8 +1,50 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  place-content: center;
+  place-items: center;
   width: 304px;
   height: 568px;
   background-color: #fff;
   border-radius: 8px;
+  font-family: Inter;
+  font-style: normal;
+`;
+
+export const ProductTitle = styled.span`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const ProductImg = styled.img`
+  width: 240px;
+  height: 240px;
+`;
+
+export const FullPriceStyled = styled.span`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -0.005em;
+  text-decoration-line: line-through;
+  color: #b5b5b6;
+`;
+
+export const CurrentPriceStyled = styled.span`
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+  letter-spacing: -0.005em;
+  color: #da4b4f;
+`;
+
+export const PaymentOptions = styled.span`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.005em;
+  color: #848587;
 `;
