69dbdd395eb2b98895d59d3ab1abcb4599d4ad67
Sophie <sophiequines@gmail.com>
2021-09-01 16:45:50 -0300

    Creating raw card and background style.

---
+++ src/components/SimpleCard/index.tsx
@@ -0,0 +1,8 @@
+//Styles
+import * as S from "./style";
+
+const SimpleCard = () => {
+  return <S.CardContainer>Hellow, I'm a card uwu</S.CardContainer>;
+};
+
+export default SimpleCard;
---
+++ src/components/SimpleCard/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  width: 304px;
+  height: 568px;
+  background-color: #fff;
+  border-radius: 8px;
+`;
